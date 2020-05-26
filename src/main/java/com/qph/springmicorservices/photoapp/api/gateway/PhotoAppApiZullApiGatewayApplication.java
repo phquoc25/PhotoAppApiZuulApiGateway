36b33948abe1f7b37704cb6d72d8ae5ad8335ce6
Quoc PHAN <phquoc25@gmail.com>
2020-05-26 22:40:21 +0700

    Initialize the project

---
+++ src/main/java/com/qph/springmicorservices/photoapp/api/gateway/PhotoAppApiZullApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.qph.springmicorservices.photoapp.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppApiZullApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiZullApiGatewayApplication.class, args);
+	}
+
+}
